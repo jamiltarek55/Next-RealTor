a8854ac0149df0035dc13df244ac06bfe66f7869
jamiltarek55 <77003619+jamiltarek55@users.noreply.github.com>
2022-09-12 09:52:17 +0600

    Update [id].js

--- pages/property/[id].js
+++ pages/property/[id].js
@@ -4,7 +4,6 @@ import { FaBed, FaBath } from 'react-icons/fa';
 import { BsGridFill } from 'react-icons/bs';
 import { GoVerified } from 'react-icons/go';
 import millify from 'millify';
-
 import { baseUrl, fetchApi } from '../../utils/fetchApi';
 import ImageScrollbar from '../../components/ImageScrollbar';
 
