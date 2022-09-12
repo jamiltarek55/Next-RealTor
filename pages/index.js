4e60db9ae7169f8dbfd8c2f76acb73ac0b151ff6
jamiltarek55 <77003619+jamiltarek55@users.noreply.github.com>
2022-09-12 09:52:56 +0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import Link from 'next/link';
 import Image from 'next/image';
 import { Flex, Box, Text, Button } from '@chakra-ui/react';
-
 import Property from '../components/Property';
 import { baseUrl, fetchApi } from '../utils/fetchApi';
 
