0d1cd3e717f88bbd700e1cc3b05b5002daf911cd
jamiltarek55 <77003619+jamiltarek55@users.noreply.github.com>
2022-09-12 09:52:37 +0600

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,6 @@ import Router from 'next/router';
 import Head from 'next/head';
 import NProgress from 'nprogress';
 import { ChakraProvider } from '@chakra-ui/react';
-
 import Layout from '../components/Layout';
 
 function MyApp({ Component, pageProps }) {
