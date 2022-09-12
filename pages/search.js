0bcdecfbb3b81508d7beed9903405dd5a2a676e5
jamiltarek55 <77003619+jamiltarek55@users.noreply.github.com>
2022-09-12 09:53:20 +0600

    Update search.js

--- pages/search.js
+++ pages/search.js
@@ -3,7 +3,6 @@ import { useRouter } from 'next/router';
 import Image from 'next/image'
 import { Flex, Box, Text, Icon } from '@chakra-ui/react';
 import { BsFilter } from 'react-icons/bs';
-
 import Property from '../components/Property';
 import SearchFilters from '../components/SearchFilters';
 import { baseUrl, fetchApi } from '../utils/fetchApi';
@@ -67,5 +66,4 @@ export async function getServerSideProps({ query }) {
     },
   };
 }
-
 export default Search;
